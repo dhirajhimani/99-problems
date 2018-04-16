58c70adb4471f1cb5472b86006504b4d384738f2
dhirajhimani <hdhiraj@thoughtworks.com>
2018-04-16 10:35:24 +0530

    P01 solution and test

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P01.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P01.scala
@@ -7,11 +7,15 @@ object P01 {
   /*
   Using the built in last function of a list
    */
-  def last[T](list: List[T]): T = ???
+  def last[T](list: List[T]): T = list.last
 
   /*
   Using recursion we ignore the first element until we have a list with only one element
    */
-  def lastRecursion[T](list: List[T]): T = ???
+  def lastRecursion[T](list: List[T]): T = list match {
+    case x :: Nil => x
+    case _ :: xs => lastRecursion(xs)
+    case _ => throw new NoSuchElementException("Empty List!")
+  }
 
 }
--- scala/src/test/scala/com/scalapractices/ninetynine_problems/_01_lists/P01Test.scala
+++ scala/src/test/scala/com/scalapractices/ninetynine_problems/_01_lists/P01Test.scala
@@ -4,6 +4,9 @@ import org.scalatest.{FunSpec, Matchers}
 
 class P01Test extends FunSpec with Matchers {
 
+  // describe -- subject under test
+  // it -- beginning of a sentence about a bit of the subject's behavior
+
   describe("P01 List builtin method") {
     it("should find last element") {
       val last = P01.last(List("a", "b", "c", "d"))
@@ -16,6 +19,13 @@ class P01Test extends FunSpec with Matchers {
       val last = P01.lastRecursion(List("a", "b", "c", "d"))
       last should be("d")
     }
+
+    it("should throw exception for empty list") {
+      intercept[NoSuchElementException] {
+        P01.lastRecursion(Nil)
+      }
+    }
+
   }
 
 }
