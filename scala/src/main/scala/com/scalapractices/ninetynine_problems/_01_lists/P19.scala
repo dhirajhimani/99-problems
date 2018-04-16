c96a34f6ad9ca77f99e3c6fdd2d4eab184165a27
dhirajhimani <hdhiraj@thoughtworks.com>
2018-04-16 14:29:01 +0530

    Created Problems for list

---
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P19.scala
@@ -0,0 +1,22 @@
+package com.scalapractices.ninetynine_problems._01_lists
+
+import scala.annotation.tailrec
+
+/**
+  * (**) Rotate a list N places to the left
+  */
+object P19 {
+
+  /*
+  Using split function defined in P17
+   */
+  def rotate[T](list: List[T], n: Int): List[T] = ???
+  /*
+    Using recursion
+     */
+  def rotateR[T](list: List[T], n: Int): List[T] = ???
+
+  private def makeNPositive[T](list: List[T], n: Int): Int = ???
+
+
+}
