fd4a330e918ad44ba67ca9be92fc4d559a81491c
dhirajhimani <hdhiraj@thoughtworks.com>
2018-04-17 19:04:59 +0530

    P02 solved

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P02.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P02.scala
@@ -2,7 +2,13 @@ package com.scalapractices.ninetynine_problems._01_lists
 
 object P02 {
 
-  def secondLast[T](list: List[T]): T = ???
+  def secondLast[T](list: List[T]): T = list match {
+    case x :: (_ :: Nil) => x
+    case _ :: xs => secondLast1(xs)
+    case _ => exception
+  }
 
-  def secondLast1[T](list: List[T]): T = ???
+  def secondLast1[T](list: List[T]): T = if (list.isEmpty || list.init.isEmpty) exception else list.init.last
+
+  def exception = throw new NoSuchElementException("Can't find secondLast element from a list with less than 2 elements")
 }
