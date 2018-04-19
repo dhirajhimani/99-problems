b080d2df3d5756985dee561b0894e99254c6e223
dhirajhimani <hdhiraj@thoughtworks.com>
2018-04-19 19:57:54 +0530

    P09 Solved

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P09.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P09.scala
@@ -16,21 +16,52 @@ object P09 {
   /*
   Using dropWhile and takeWhile
    */
-  def pack[T](list: List[T]): List[List[T]] = ???
+  def pack[T](list: List[T]): List[List[T]] = list match {
+    case x :: xs => (x +: xs.takeWhile(el => el == x)) +: pack(xs.dropWhile(el => el == x))
+    case Nil => Nil
+  }
+
   /*
   tailrec implementation of the above code
    */
-  def pack_tailrecursive[T](list: List[T]): List[List[T]] = ???
+  def pack_tailrecursive[T](list: List[T]): List[List[T]] = {
+    @tailrec
+    def go(input: List[T], result: List[List[T]]): List[List[T]] = {
+      input match {
+        case Nil => result
+        case x :: xs => go(input.dropWhile(y => y == x), (result :+ input.takeWhile(y => y == x)))
+      }
+    }
+    go(list, List[List[T]]())
+  }
 
   /*
   Using in-built function span
    */
-  def pack_span[T](list: List[T]): List[List[T]] = ???
+  def pack_span[T](list: List[T]): List[List[T]] = {
+    list match {
+      case x :: xs => {
+        val t = xs.span(y => y ==x)
+        (x +: t._1) +: pack_span(t._2)
+      }
+      case Nil => Nil
+    }
+  }
 
   /*
   Using in-built function span tail rec
    */
-  def pack_span_tailrec[T](list: List[T]): List[List[T]] = ???
+  def pack_span_tailrec[T](list: List[T]): List[List[T]] = {
+    def packR(l: List[T], result: List[List[T]]): List[List[T]] = l match {
+      case x :: xs => {
+        val t = xs.span(y => y ==x)
+        packR(t._2, result :+ (x +: t._1))
+      }
+      case Nil => result
+    }
+    packR(list, List())
+  }
+
 
 
 }
