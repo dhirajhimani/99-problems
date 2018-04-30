f39b75f3195687d7186a93a2ff0fdd86bfe7a7db
dhirajhimani <hdhiraj@thoughtworks.com>
2018-04-30 10:40:50 +0530

    Solved Problem P13

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P13.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P13.scala
@@ -14,9 +14,25 @@ object P13 {
   /**
     * Using dropWhile method and tail recursion
     */
-  def encode_direct[T](list: List[T]): List[Any] = ???
+  def encode_direct[T](list: List[T]): List[Any] = list match {
+    case x :: xs => {
+      val ls = list.takeWhile(_ == x)
+      if (ls.size > 1) {
+        (ls.size, ls.head) :: encode_direct(xs.dropWhile(_ == x))
+      } else {
+        ls.head :: encode_direct(xs.dropWhile(_ == x))
+      }
+    }
+    case Nil => Nil
+  }
 
-  def encode_direct_span[T](list: List[T]): List[Any] = ???
+  def encode_direct_span[T](list: List[T]): List[Any] = list match {
+    case x :: xs => {
+      val t = list.span(_ == x)
+      (t._1.size, t._1.head) :: encode_direct_span(t._2)
+    }
+    case Nil => Nil
+  }
 
 
 }
