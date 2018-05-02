36d0717e357d7a399fd6b5e41d34db04028c895c
dhirajhimani <hdhiraj@thoughtworks.com>
2018-05-02 11:15:33 +0530

    Solved Problem P17

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P17.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P17.scala
@@ -8,8 +8,21 @@ package com.scalapractices.ninetynine_problems._01_lists
   */
 object P17 {
 
-  def split[T](list: List[T], n: Int): (List[T], List[T]) = ???
+  def split[T](list: List[T], n: Int): (List[T], List[T]) = {
+    def go(list: List[T], count: Int, result: List[T]): (List[T], List[T]) = list match {
+      case x :: xs if (count > 0)=>  go(xs, count - 1, x :: result)
+      case x :: xs => (result.reverse, list)
+    }
 
-  def split_take_drop[T](list: List[T], n: Int): (List[T], List[T]) = ???
+    if (n ==0) (Nil, list) else go(list, n, Nil)
+  }
+
+  def splitUsingSplitAt[T](list: List[T], n: Int): (List[T], List[T]) = {
+    list.splitAt(n)
+  }
+
+  def split_take_drop[T](list: List[T], n: Int): (List[T], List[T]) = {
+    (list.take(n), list.drop(n))
+  }
 
 }
--- scala/src/test/scala/com/scalapractices/ninetynine_problems/_01_lists/P17Test.scala
+++ scala/src/test/scala/com/scalapractices/ninetynine_problems/_01_lists/P17Test.scala
@@ -23,6 +23,25 @@ class P17Test extends FunSpec with Matchers {
     }
   }
 
+  describe("P17 Spec using splitAt") {
+
+    it("should split into two halves by length") {
+      val (first, second) = P17.splitUsingSplitAt(List("a", "b", "c", "d", "e", "f", "g", "h", "i", "k"), 3)
+      first should have length 3
+      first should be(List("a", "b", "c"))
+      second should have length 7
+      second should be(List("d", "e", "f", "g", "h", "i", "k"))
+    }
+
+    it("should split into two halves by length with size 0 and list size when n is 0") {
+      val (first, second) = P17.splitUsingSplitAt(List("a", "b", "c", "d", "e", "f", "g", "h", "i", "k"), 0)
+      first should have length 0
+      first should be(List())
+      second should have length 10
+      second should be(List("a", "b", "c", "d", "e", "f", "g", "h", "i", "k"))
+    }
+  }
+
   describe("P17 Spec take-drop") {
 
     it("should split into two halves by length") {
