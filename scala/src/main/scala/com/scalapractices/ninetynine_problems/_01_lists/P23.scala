4ab5a09ecc2b2fa1bd6b4ccdaf8bc1ea67467082
dhirajhimani <hdhiraj@thoughtworks.com>
2018-05-03 11:37:04 +0530

    Solved Problem P23

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P23.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P23.scala
@@ -11,8 +11,14 @@ object P23 {
   Easy solution by generating a random sequence of numbers.
   This solution may not be ideal as this can produce duplicates
    */
-  def randomSelect[T](list: List[T], n: Int): List[T] = ???
+  def randomSelect[T](list: List[T], n: Int): List[T] = {
+    val ls = List.fill(n)(Random.nextInt(list.size))
+    ls.map(list(_))
+  }
 
-  def randomSelectR[T](list: List[T], n: Int): List[T] = ???
+  def randomSelectR[T](list: List[T], n: Int): List[T] = n match {
+    case 0 => Nil
+    case _ => list(Random.nextInt(list.size)) :: randomSelect(list, n-1)
+  }
 
 }
--- scala/src/test/scala/com/shekhargulati/ninetynine_problems/_01_lists/P23Test.scala
+++ scala/src/test/scala/com/shekhargulati/ninetynine_problems/_01_lists/P23Test.scala
@@ -13,7 +13,7 @@ class P23Test extends FunSpec with Matchers {
 
   describe("P23 Spec recursive without duplicates") {
     it("should randomly select three elements") {
-      val randomElements: List[String] = P23.randomSelect(List("a", "b", "c", "d", "e", "f", "g", "h"), 3)
+      val randomElements: List[String] = P23.randomSelectR(List("a", "b", "c", "d", "e", "f", "g", "h"), 3)
       println(randomElements)
       randomElements should have length 3
     }
