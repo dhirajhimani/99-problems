10c9b50259ecd440272db95c8e784bbb35eb80f9
dhirajhimani <hdhiraj@thoughtworks.com>
2018-05-03 09:42:24 +0530

    Solved Problem P20

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P20.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P20.scala
@@ -8,12 +8,25 @@ object P20 {
   /*
   Using built in take drop
    */
-  def removeAt[T](list: List[T], k: Int): (List[T], T) = ???
+  def removeAt[T](list: List[T], k: Int): (List[T], T) = {
+    val removedEle = list.take(k).last
+    (list.take(k-1) ++ list.drop(k), removedEle)
+  }
 
-  def removeAt0[T](list: List[T], k: Int): (List[T], T) = ???
+  def removeAt0[T](list: List[T], k: Int): (List[T], T) = {
+    def go(list: List[T], k: Int): List[T] = list match {
+      case x :: xs if(k !=0) => x :: go(xs, k -1)
+      case x :: xs => xs
+      case Nil => Nil
+    }
+    (go(list, k), list(k))
+  }
 
   /*
   Using splitAt
    */
-  def removeAt_splitAt[T](list: List[T], k: Int): (List[T], T) = ???
+  def removeAt_splitAt[T](list: List[T], k: Int): (List[T], T) = {
+    val (start, end) = list.splitAt(k)
+    (start.init ::: end, start.last)
+  }
 }
