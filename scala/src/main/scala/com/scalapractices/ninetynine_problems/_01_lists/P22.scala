5ef9183036110dfb1974eb02ad014f8f495ccf27
dhirajhimani <hdhiraj@thoughtworks.com>
2018-05-03 11:25:08 +0530

    Solved Problem P22

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P22.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P22.scala
@@ -7,8 +7,16 @@ import scala.annotation.tailrec
   */
 object P22 {
 
-  def range(start: Int, end: Int): List[Int] = ???
+  def range(start: Int, end: Int): List[Int] = (start to end).toList
 
-  def rangeR(start: Int, end: Int): List[Int] = ???
+  def rangeR(start: Int, end: Int): List[Int] = {
+    def go (list: List[Int], start: Int, end: Int, by: Int): List[Int] = {
+      (start, end) match {
+        case (s,e) if e > 0 => go((list.head + by) :: list, s , e - by, by)
+        case _ => list
+      }
+    }
+    go(List(start), start, end - start, 1).reverse
+  }
 
 }
