214eb10757dd9190cfcfea1cd1b863b6780716b2
dhirajhimani <hdhiraj@thoughtworks.com>
2018-05-03 10:28:52 +0530

    Solved Problem P21

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P21.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P21.scala
@@ -8,10 +8,22 @@ import scala.collection.mutable
   */
 object P21 {
 
-  def insertAt[T](list: List[T], pos: Int, t: T): List[T] = ???
+  def insertAt[T](list: List[T], pos: Int, t: T): List[T] = {
+    list.take(pos-1) ++ (t :: list.drop((pos-1)))
+  }
 
-  def insertAt_inbuilt[T](list: List[T], pos: Int, t: T): List[T] = ???
+  def insertAt_inbuilt[T](list: List[T], pos: Int, t: T): List[T] = {
+    if(pos < 1) {
+      throw new NoSuchElementException("pos less than 1")
+    }
+    val lsBuffer: mutable.Buffer[T] = list.toBuffer
+    lsBuffer.insert(pos - 1, t)
+    lsBuffer.toList
+  }
 
-  def insertAt_recursion[T](list: List[T], pos: Int, t: T): List[T] = ???
+  def insertAt_recursion[T](list: List[T], pos: Int, t: T): List[T] = list match {
+    case x :: xs if (pos > 1) => x :: insertAt_recursion(xs , pos-1, t)
+    case _ if(pos == 1) => t :: list
+  }
 
 }
