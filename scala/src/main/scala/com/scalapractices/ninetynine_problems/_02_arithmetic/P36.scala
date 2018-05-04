69a53555cd72adc045eb67daf9982a6e9701a5ac
dhirajhimani <hdhiraj@thoughtworks.com>
2018-05-04 14:41:44 +0530

    Create Problems for Arithmetic

---
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_02_arithmetic/P36.scala
@@ -0,0 +1,13 @@
+package com.scalapractices.ninetynine_problems._02_arithmetic
+
+/**
+  * (**) A list of Goldbach compositions.
+  */
+object P36 {
+
+  def goldbach_list(range: Range): List[(Int, (Int, Int))] = ???
+
+  def goldbach_list1(range: Range, gt: Int): List[(Int, (Int, Int))] = ???
+
+
+}
---
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_02_arithmetic/P37.scala
@@ -0,0 +1,9 @@
+package com.scalapractices.ninetynine_problems._02_arithmetic
+
+/**
+  * (**) Determine the greatest common divisor of two positive integer numbers.
+  */
+object P37 {
+
+  def gcd(first: Int, second: Int): Int = ???
+}
