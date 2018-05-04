abc518b388529d1b63d107f81f0ab1c03dd04f2e
dhirajhimani <hdhiraj@thoughtworks.com>
2018-05-04 11:21:53 +0530

    Solved Problem P24

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P24.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_01_lists/P24.scala
@@ -1,10 +1,17 @@
 package com.scalapractices.ninetynine_problems._01_lists
 
+import scala.util.Random
+
 /**
   * (*) Lotto: Draw N different random numbers from the set 1..M
   */
 object P24 {
 
-  def randomSelect(n: Int, range: (Int, Int)): List[Int] = ???
+  def randomSelect(n: Int, range: (Int, Int)): List[Int] = {
+    n match {
+      case 0 => Nil
+      case _ => (Random.nextInt(range._2) + range._1) :: randomSelect(n-1, range)
+    }
+  }
 
 }
