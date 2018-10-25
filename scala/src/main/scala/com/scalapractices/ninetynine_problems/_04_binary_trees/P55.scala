7a3532bbc0ad6e4941d538eccd899c71a67f9592
Dhiraj Himani <dhrjkmr538@gmail.com>
2018-10-25 12:59:36 +0530

    dummy test for tree

--- scala/src/main/scala/com/scalapractices/ninetynine_problems/_04_binary_trees/P55.scala
+++ scala/src/main/scala/com/scalapractices/ninetynine_problems/_04_binary_trees/P55.scala
@@ -2,6 +2,6 @@ package com.scalapractices.ninetynine_problems._04_binary_trees
 
 object P55 {
 
-  def cBalanced(nodes: Int, value: String): Tree[String] = Tree(value, End, End)
+  def cBalanced(nodes: Int, value: String): Tree[String] = Node(value, End, End)
 
 }
---
+++ scala/src/test/scala/com/scalapractices/ninetynine_problems/_04_binary_trees/P55Test.scala
@@ -0,0 +1,12 @@
+package com.scalapractices.ninetynine_problems._04_binary_trees
+
+import org.scalatest.{FunSpec, Matchers}
+
+class P55Test extends FunSpec with Matchers {
+
+  describe("P55 Tree balanced Tree") {
+    it("should provide Balanced Tree") {
+      4 should be(2 * 2)
+    }
+  }
+}
