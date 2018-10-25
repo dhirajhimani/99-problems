b8337a89d4e1f2dd3405375002480f948c84bdad
Dhiraj Himani <dhrjkmr538@gmail.com>
2018-10-25 13:17:34 +0530

    initial master test

---
+++ scala/src/test/scala/com/scalapractices/ninetynine_problems/_04_binary_trees/P55Test.scala
@@ -0,0 +1,10 @@
+package com.scalapractices.ninetynine_problems._04_binary_trees
+
+import org.scalatest.{FunSuite, Matchers}
+
+class P55Test extends FunSuite with Matchers {
+  test("testCBalanced") {
+    2 should be(2)
+  }
+
+}
